983c3c5e416285c2eca9a7d54ac78ee0726a9ee0
renasvgp <100416810+renasvgp@users.noreply.github.com>
2023-03-07 09:58:43 +0100

    Add files via upload

---
+++ OblOpg1/Car.cs
@@ -0,0 +1,33 @@
+﻿namespace OblOpg1;
+
+public class Car
+{
+    public int Id { get; set; }
+
+    public string? model { get; set; }
+
+    public int price { get; set; }
+
+    public string? licensePlate { get; set; }
+
+    public bool ValidateId()
+    {
+        return Id >= 1;
+    }
+
+    public bool ValidateModel()
+    {
+        return model?.Length >= 4;
+    }
+
+    public bool ValidatePrice()
+    {
+        return price > 0;
+    }
+
+    public bool ValidateLicensePlate()
+    {
+        return licensePlate?.Length is >= 2 and <= 7;
+    }
+}
+
